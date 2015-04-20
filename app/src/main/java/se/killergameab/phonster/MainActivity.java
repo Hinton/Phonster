c49834f1e7aaa976e7691719f93a743ab0c31040
Aleksandar <aleks.zezovski@gmail.com>
2015-04-20 12:31:21 +0200

    Battle screen

--- app/src/main/java/se/killergameab/phonster/MainActivity.java
+++ app/src/main/java/se/killergameab/phonster/MainActivity.java
@@ -63,4 +63,9 @@ public class MainActivity extends ActionBarActivity {
         Intent i = new Intent(getApplicationContext(), AttackActivity.class);
         startActivity(i);
     }
+
+    public void onBattleClick(View view) {
+        Intent i = new Intent(getApplicationContext(), BattleActivity.class);
+        startActivity(i);
+    }
 }
