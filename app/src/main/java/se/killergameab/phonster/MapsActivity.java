f84fd78de88599491fea515406c24c58cb797daa
Hanna Hultgren <hanna.hultgren@gmail.com>
2015-05-18 23:31:42 +0200

    Changed back map timer values

--- app/src/main/java/se/killergameab/phonster/MapsActivity.java
+++ app/src/main/java/se/killergameab/phonster/MapsActivity.java
@@ -3,7 +3,6 @@ package se.killergameab.phonster;
 import android.content.Context;
 import android.content.Intent;
 import android.location.Location;
-import android.location.LocationListener;
 import android.os.CountDownTimer;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
@@ -133,7 +132,7 @@ public class MapsActivity extends FragmentActivity implements GoogleMap.OnMyLoca
         gMap.setOnMyLocationChangeListener(this);
 
         Random r = new Random();
-        int timeUntilNextMonster = r.nextInt(5000 - 2000) + 2000;
+        int timeUntilNextMonster = r.nextInt(35000 - 20000) + 20000;
         new CountDownTimer(timeUntilNextMonster, 1000) {
 
             public void onTick(long millisUntilFinished) {
